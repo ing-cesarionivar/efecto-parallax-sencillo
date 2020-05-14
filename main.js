2cfe561b586aabc5aaf53eb5dc61a9e60aaaa29b
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-05-13 23:01:11 -0400

    Leve modificacion al Js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     $(window).scroll(function(){
         var barra = $(window).scrollTop();
-        var posicion = barra * 0.10;
+        var posicion = barra * 0.05;
 
         $('body').css({
             'background-position': '0 -' + posicion + 'px'
